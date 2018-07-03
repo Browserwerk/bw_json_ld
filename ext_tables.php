37efd41a0b59b3666bbb996c87a938eb21c6c388
Jeremie Constant <mail@jeremieconstant.de>
2018-07-03 13:12:51 +0000

    [Task] implement plain Backend-Module

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,17 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3_MODE') || die('Access denied.');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'JeremieConstant.JctJsonLd',
+    'web', // Make module a submodule of 'web'
+    'jsongenerator', // Submodule key
+    '', // Position
+    [
+
+    ],
+    [
+        'access' => 'user,group',
+        'icon'   => 'EXT:jct_json_ld/Resources/Public/Icons/module-json.png',
+        'labels' => 'LLL:EXT:jct_json_ld/Resources/Private/Language/locallang_jsongenerator.xlf',
+    ]
+);
+
